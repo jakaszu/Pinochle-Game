90d74c20ae71907664cf5804be90a60d8317e6d1
allen <allen@LAPTOP-PNGI701P>
2019-01-14 13:36:22 -0600

    removed unnecessary generic

--- src/pinochle/entity/Hand.java
+++ src/pinochle/entity/Hand.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    private final List<Card> cards = new ArrayList<Card>();
+    private final List<Card> cards = new ArrayList<>();
 
     public List<Card> getCards() {
         return cards;
